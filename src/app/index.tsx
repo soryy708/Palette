0095587134185f575ed5191e8957163e3bc2f500
Climax708 <soryy708@gmail.com>
2021-03-10 21:26:06 +0200

    Add to wheel shadow & reposition

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -70,7 +70,7 @@ const App: React.FunctionComponent = () => {
                 Add
             </button>
         </div>
-        <div style={{position: 'absolute', top: '0', right: '0', borderRadius: '50%'}}>
+        <div style={{position: 'absolute', top: '0.5em', right: '0.5em', boxShadow: '0 0 8px #000', borderRadius: '50%'}}>
             <ColorWheel
                 items={stripes.map(stripe => stripe.color)}
                 size='8em'
