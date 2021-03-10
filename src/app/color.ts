4a038d211e228f22e3cb64bf661a03b3e5bbc101
Climax708 <soryy708@gmail.com>
2021-03-10 20:55:51 +0200

    Refactor

--- src/app/color.ts
+++ src/app/color.ts
@@ -3,3 +3,44 @@ export type Color = {
     g: number;
     b: number;
 };
+
+/**
+ * 
+ * @param colorChannel between 0 and 255
+ * @returns linearized value between 0.0 and 1.0
+ */
+export const linearizeColor = (colorChannel: number): number => {
+    if (colorChannel/255 <= 0.04045) {
+        return colorChannel/255 / 12.92;
+    } else {
+        return Math.pow((colorChannel/255 + 0.055) / 1.055, 2.4);
+    }
+};
+
+/**
+ * 
+ * @param r between 0 and 255
+ * @param g between 0 and 255
+ * @param b between 0 and 255
+ * @returns between 0.0 and 1.0
+ */
+const rgbToLuminance = (r: number, g: number, b: number): number =>
+    0.2126 * linearizeColor(r) +
+    0.7152 * linearizeColor(g) +
+    0.0722 * linearizeColor(b);
+
+export const colorToLuminance = (color: Color): number => rgbToLuminance(color.r, color.g, color.b);
+
+/**
+ * 
+ * @param luminance between 0.0 and 1.0
+ * @returns between 0 and 100
+ */
+export const luminanceToPerceivedLightness = (luminance: number): number => {
+    if (luminance <= (216/24389)) {
+        return luminance * (24389/27);
+    } else {
+        return Math.pow(luminance, (1/3)) * 116 - 16;
+    }
+};
+
--- src/app/stripes/stripe.tsx
+++ src/app/stripes/stripe.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Color } from '../color';
+import { Color, luminanceToPerceivedLightness, colorToLuminance } from '../color';
 
 type StripeProps = {
     color: Color;
@@ -15,45 +15,8 @@ const Stripe: React.FunctionComponent<StripeProps> = (props: StripeProps) => {
         }, 10);
     }, []);
 
-    const getBackgroundColor = () => ({r: props.color.r, g: props.color.g, b: props.color.b});
+    const getBackgroundColor = () => props.color;
     const getTextColor = (): Color => {
-        /**
-         * 
-         * @param colorChannel between 0 and 255
-         * @returns linearized value between 0.0 and 1.0
-         */
-        const linearizeColor = (colorChannel: number) => {
-            if (colorChannel/255 <= 0.04045) {
-                return colorChannel/255 / 12.92;
-            } else {
-                return Math.pow((colorChannel/255 + 0.055) / 1.055, 2.4);
-            }
-        };
-
-        /**
-         * 
-         * @param r between 0 and 255
-         * @param g between 0 and 255
-         * @param b between 0 and 255
-         * @returns between 0.0 and 1.0
-         */
-        const rgbToLuminance = (r: number, g: number, b: number): number =>
-            0.2126 * linearizeColor(r) +
-            0.7152 * linearizeColor(g) +
-            0.0722 * linearizeColor(b);
-        const colorToLuminance = (color: Color): number => rgbToLuminance(color.r, color.g, color.b);
-        /**
-         * 
-         * @param luminance between 0.0 and 1.0
-         * @returns between 0 and 100
-         */
-        const luminanceToPerceivedLightness = (luminance: number): number => {
-            if (luminance <= (216/24389)) {
-                return luminance * (24389/27);
-            } else {
-                return Math.pow(luminance, (1/3)) * 116 - 16;
-            }
-        };
         const backgroundPerceivedLightness = luminanceToPerceivedLightness(colorToLuminance(getBackgroundColor()));
         if (backgroundPerceivedLightness > 50) {
             return {r: 0, g: 0, b: 0};
