1cafac958a6d4490ef8fcf4292e9f6100da39c68
Climax708 <soryy708@gmail.com>
2021-03-10 21:17:06 +0200

    Refactor

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -10,54 +10,61 @@ const App: React.FunctionComponent = () => {
         let autoInc = 0;
         return () => autoInc++;
     })();
+
     const getRandomColor = (): Color => ({
         r: Math.floor(Math.random() * 255),
         g: Math.floor(Math.random() * 255),
         b: Math.floor(Math.random() * 255),
     });
 
+    const removeStripe = (stripeIndex: number) => {
+        const stripe = stripes[stripeIndex];
+        setStripes(prevStripes => {
+            const index = prevStripes.findIndex(it => it.id === stripe.id);
+            if (index === -1) {
+                return prevStripes;
+            }
+            return [...prevStripes.slice(0, index), ...prevStripes.slice(index + 1)];
+        });
+    };
+
+    const rerollStripe = (stripeIndex: number) => {
+        const stripe = stripes[stripeIndex];
+        setStripes(prevStripes => {
+            const index = prevStripes.findIndex(it => it === stripe);
+            if (index === -1) {
+                return prevStripes;
+            }
+            return [
+                ...prevStripes.slice(0, index),
+                {
+                    ...prevStripes[index],
+                    color: getRandomColor(),
+                },
+                ...prevStripes.slice(index + 1),
+            ];
+        });
+    };
+
+    const addStripe = () => {
+        setStripes(prevStripes => [...prevStripes, {
+            id: generateId(),
+            color: getRandomColor(),
+        }]);
+    };
+
     return <div className="page flexColumn">
         <div className="pageTop flexColumn">
             <Stripes
                 items={stripes}
-                onRemove={itemIndex => {
-                    const item = stripes[itemIndex];
-                    setStripes(prevStripes => {
-                        const index = prevStripes.findIndex(it => it.id === item.id);
-                        if (index === -1) {
-                            return prevStripes;
-                        }
-                        return [...prevStripes.slice(0, index), ...prevStripes.slice(index + 1)];
-                    });
-                }}
-                onReroll={itemIndex => {
-                    const item = stripes[itemIndex];
-                    setStripes(prevStripes => {
-                        const index = prevStripes.findIndex(it => it === item);
-                        if (index === -1) {
-                            return prevStripes;
-                        }
-                        return [
-                            ...prevStripes.slice(0, index),
-                            {
-                                ...prevStripes[index],
-                                color: getRandomColor(),
-                            },
-                            ...prevStripes.slice(index + 1),
-                        ];
-                    });
-                }}
+                onRemove={itemIndex => removeStripe(itemIndex)}
+                onReroll={itemIndex => rerollStripe(itemIndex)}
             />
         </div>
         <div className="pageBottom">
             <button
                 type="button"
-                onClick={() => {
-                    setStripes(prevStripes => [...prevStripes, {
-                        id: generateId(),
-                        color: getRandomColor(),
-                    }]);
-                }}
+                onClick={addStripe}
                 style={{width: '100%'}}
             >
                 Add
